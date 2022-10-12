e015b0f4f226d93535cf1a3bd065c73cdf5a4e05
Auraxium <clutchmasterman@gmail.com>
2022-10-12 12:14:13 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import Homepage from './components/Homepage'
 import Navbar from './components/Navbar';
 import BuyDailyPass from './components/BuyDailyPass';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import { Link } from 'react-router-dom';
-import { Button } from 'bootstrap';
-import {useNavigate} from 'react-router-dom';
 
 
 function App() {
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { Link, useNavigate } from 'react-router-dom';
-
-
+import { Link, useNavigate } from "react-router-dom";
 
 function Homepage() {
   let nav = useNavigate();
   return (
     <>
-     
-    
       <div className="row col-12">
-      <button className="btn btn-primary" onClick={() => nav('/BuyDailyPass')}>Buy Daily Pass</button>
-      <button className="btn btn-success">Extend Pass</button>
-      <button className="btn btn-warning">Locate lot</button>
-      <button className="btn btn-danger">Purchase Semester Pass</button>
+        <button
+          className="btn btn-primary"
+          onClick={() => nav("/BuyDailyPass")}
+        >
+          Buy Daily Pass
+        </button>
+        <button className="btn btn-success">Extend Pass</button>
+        <button className="btn btn-warning">Locate lot</button>
+        <button className="btn btn-danger">Purchase Semester Pass</button>
         <div>asd</div>
       </div>
     </>
