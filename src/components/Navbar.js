910bc7bf55780aa1590757e10e1e390a5bf79a1f
Auraxium <clutchmasterman@gmail.com>
2022-11-02 10:21:20 -0700

    ui stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,11 @@ import { Link,useNavigate } from 'react-router-dom';
 export default function Navbar() {
   let nav = useNavigate()
   return (
-    <div className="row bg-danger mb-2 p-1 text-white">
+   <>
+    <div className="bg-danger mb-2 p-1 text-white">
       <h1 className="col-2" style={{"cursor": "pointer"}} onClick={() => nav("/")}>MataPark</h1>
     </div>
+  </>
 
   );
 }
