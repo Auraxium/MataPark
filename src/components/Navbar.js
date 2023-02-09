7a8dced252b046eda733fdf5aa785db3e06f7fe0
DigitalSpice <lmcurry93@hotmail.com>
2023-02-08 21:42:59 -0800

    Clock Change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
     <>
       <div className="bg-danger mb-2 p-1 d-flex text-white  mx-0 align-items-center sticky-top" style={{justifyContent: 'space-between'}}>
         <h1 className="col-auto" style={{"cursor": "pointer"}} onClick={() => {nav('/'); window.location.reload(false)}}>MataPark</h1>
-        <Clock format={'ddd, MMM Mo, h:mm A'} ticking={true} timezone={'US/Pacific'}/>
+        <Clock format={'h:mm:ss A'} ticking={true} timezone={'US/Pacific'} style={{fontSize: '1.5em'}}/>
         <h2 className="col-auto" style={{"cursor": "pointer"}} onClick={() => nav("/Login")}>Login</h2>
       </div>
     </>
