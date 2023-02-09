0031d7ae68ec1c181a92b5fe3a3997104ecc912d
Auraxium <clutchmasterman@gmail.com>
2023-02-08 20:09:08 -0800

    nvm

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Calculate from './components/Calculate';
 import Login from './components/Login';
 import LocateLot from './components/LocateLot';
 import EVTransportation from './components/EVTransportation';
+import ParkingTimer from './components/ParkingTimer'
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 
