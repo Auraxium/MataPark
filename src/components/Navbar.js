690fc07ed32db2aae6203b5fcd2aae64e77e4070
= <=>
2023-02-09 16:28:19 -0800

    centered timer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
         className="bg-danger mb-2 p-1 d-flex text-white  mx-0 align-items-center sticky-top"
         style={{ justifyContent: "space-between" }}
       >
-        <div className="col-4 row">
+        <div className="col row">
           <h1
             className="col-auto"
             style={{ cursor: "pointer" }}
@@ -23,13 +23,14 @@ export default function Navbar() {
         </div>
 
         <Clock
-          className="col-4  text-center"
+          className="col  text-center"
           format={"h:mm:ss A"}
           ticking={true}
           timezone={"US/Pacific"}
           style={{ fontSize: "1.5em" }}
         />
-        <div className="col-4 row justify-content-end">
+
+        <div className="col row justify-content-end">
           <h2
             className="col-auto"
             style={{ cursor: "pointer" }}
