0ac78938212bcedad14a645599c5a7363d7407fe
Shashi Wong <shashiwong@Shashis-MacBook-Pro.local>
2023-02-22 11:54:26 -0800

    singIn

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,31 +3,39 @@ import React from "react";
 export default function Login() {
   return (
     <div>
-      <h1>Login form</h1>
+        <h1>Registration form</h1>
+      
+      <label for="yourname"><b>Email: </b></label>
+       <input type="text" placeholder="Enter Email" 
+       name="yourname" required></input><br/>
+<br/>
+       <label for="yourpassword"><b>Password:</b></label>
+       <input type="password" placeholder="Enter Password" 
+       name="yourpassword" required></input><br/>
+<br/>
 
-      <label for="yourname">
-        <b>Username</b>
-      </label>
-      <input
-        type="text"
-        placeholder="Enter Username"
-        name="yourname"
-        required
-      ></input>
-      <br />
-      <br />
-      <label for="yourpassword">
-        <b>Password</b>
-      </label>
-      <input
-        type="password"
-        placeholder="Enter Password"
-        name="yourpassword"
-        required
-      ></input>
-      <br />
-      <br />,
-      <button type="submit">Login</button>
+
+<br/>
+       <label for = "Parking Type">Choose Parking:
+       </label>
+       <select name="car" id ="cars">
+         <option value="GeneralParking">General Parking</option>
+         <option value="HandicapParking">Handicap</option>
+         <option value="MotorVehicle">MotorVehicle</option>
+         <option value="StaffParking">Staff Parking</option>
+       </select>
+       <br></br>
+       <br></br>
+       <br></br>
+
+      <button type="submit">Submit</button>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+<p>Or Create a login with:</p>
     </div>
   );
 }
