c91d6bdc81b26b71cb6f822ba3a1837c093fac57
DigitalSpice <lmcurry93@hotmail.com>
2023-04-26 03:17:33 -0700

    updates + street parking

---
+++ src/components/SpotAngel.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SpotAngel = () => {
+	return (
+		<iframe
+			src="https://www.spotangels.com/map?lng=-118.52750566928097&lat=34.239649&zoom=15&spLng=-118.531647&spLat=34.239649&spAddress=California%20State%20University,%2018111%20Nordhoff%20St,%20Northridge,%20California%2091325,%20United%20States"
+			title="Spot Angel"
+			width="100%"
+			height="600"
+		></iframe>
+	);
+};
+
+export default SpotAngel;
